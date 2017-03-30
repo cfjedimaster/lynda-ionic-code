f0eec55c8d0c2919c42c6d5319d4a8dd7ffb8431
Raymond Camden <raymondcamden@gmail.com>
2017-03-30 00:08:44 -0500

    more

--- 4/4/infinitescroll/src/pages/home/home.ts
+++ 4/4/infinitescroll/src/pages/home/home.ts
@@ -8,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public cats = [];
+
   constructor(public navCtrl: NavController) {
-    
+    //create some initial cats
+    for(let i=0;i<25;i++) {
+      this.cats.push('Cat '+i);
+    }
+  }
+
+  moreCats(infiniteScroll) {
+    //setTimeout trick from docs!
+    setTimeout(() => {
+      for(let i=0;i<25;i++) {
+        this.cats.push('Cat ' + (this.cats.length+1));
+      }
+      infiniteScroll.complete();
+    }, 1000);
+
   }
 
 }
